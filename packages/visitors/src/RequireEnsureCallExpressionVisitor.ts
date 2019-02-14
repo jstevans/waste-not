98f6ef65197cea70e80a92e9f23237c28b9a9770
John Stevans <johnst@microsoft.com>
2019-02-14 11:39:30 -0800

    Add RequireEnsureCallExpressionVisitor

---
+++ packages/visitors/src/RequireEnsureCallExpressionVisitor.ts
@@ -0,0 +1,41 @@
+import { Visitor } from "walker/types";
+import addDependency from "./utils/addDependency";
+import addWarning from "./utils/addWarning";
+import { CallExpression, isIdentifier, isArrayExpression, Expression, ArrayExpression, isStringLiteral } from "@babel/types";
+import isRequireMemberExpression from "./utils/isRequireMemberExpression";
+
+const DEPENDENCIES_MUST_BE_ARRAY = "The first argument of a 'require.ensure' call must be an array.";
+const DEPENDENCIES_MUST_BE_STRINGS = "The elements of the first argument of a 'require.ensure' call must be strings.";
+
+const RequireEnsureCallExpressionVisitor: Visitor<CallExpression> =
+    function visitRequireEnsureCallExpressionVisitor(node: CallExpression, state: any) {
+        if (isRequireEnsureCallExpression(node)) {
+            const dependenciesArg = node.arguments[0];
+            if (assertArrayExpression(dependenciesArg, state)) {
+                dependenciesArg.elements.forEach(element => {
+                    if (element && isStringLiteral(element)) {
+                        addDependency(element.value, state);
+                    } else {
+                        addWarning(DEPENDENCIES_MUST_BE_STRINGS, state);
+                    }
+                });
+            }
+        }
+    }
+
+function isRequireEnsureCallExpression(node: CallExpression) {
+    return isRequireMemberExpression(node.callee) &&
+        isIdentifier(node.callee.property) &&
+        node.callee.property.name === 'ensure';
+}
+
+function assertArrayExpression(node: any, state: any): node is ArrayExpression {
+    if (!isArrayExpression(node)) {
+        addWarning(DEPENDENCIES_MUST_BE_ARRAY, state);
+        return false;
+    }
+    return true;
+}
+
+
+export default RequireEnsureCallExpressionVisitor;
