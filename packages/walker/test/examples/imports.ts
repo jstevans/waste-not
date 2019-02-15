c8d1e36405ccd41b8873c8262f92e1659a33203b
John Stevans <johnst@microsoft.com>
2019-02-15 10:38:40 -0800

    Add some integration tests between walker and visitors

--- packages/walker/test/examples/imports.ts
+++ packages/walker/test/examples/imports.ts
@@ -1,17 +1,54 @@
-const lines = ["import './exports';",
-    "import foo1 from './exports';",
-    "import { default as foo2 } from './exports';",
-    "import { foo, bar } from './exports';",
-    "import { default as foo3, bar as bar1 } from './exports';",
-    "import * as foobar from './exports';",
-    "",
-    "import('./exports');",
-    "const asyncFah = async() => await import('./exports');",
-    "import('./exports').then(() => {});",
-    "",
-    "require('./exports');",
-    "",
-    "require.ensure(['./exports'], () => {});",
-    "require.context('./exports');"];
+const lines = {
+    import: [
+        "import './import';",
+        "import foo1 from './importDefault';",
+        "import { default as foo2 } from './importDefaultRename';",
+        "import { foo, bar } from './importSome';",
+        "import { default as foo3, bar as bar1 } from './importRenames';",
+        "import * as foobar from './importAll';"
+    ],
 
-export default lines.join('\n');
+    dynamicImport: [
+        "import('./dynamicImport');",
+        "import('./dynamicImport' + 'plus')",
+        "import('./dynamicImport' + x + 'wildcard')"
+    ],
+
+    awaitDynamicImport: [
+        "const async1 = async () => await import('./dynamicImportAwait');",
+        "const async2 = async () => await import('./dynamicImportAwait' + 'plus');",
+        "const async3 = async () => await import('./dynamicImportAwait' + x + 'wildcard');"
+    ],
+
+    dynamicImportThen: [
+        "import('./dynamicImportThen').then(() => {});",
+        "import('./dynamicImportThen' + 'plus').then(() => {});",
+        "import('./dynamicImportThen' + x + 'wildcard').then(() => {});"
+    ],
+
+    require: [
+        "require('./require');",
+        "require('./require' + 'plus')",
+        "require('./require' + x + 'wildcard')"
+    ],
+
+    requireResolve: [
+        "require.resolve('requireResolve');",
+        "require.resolve('requireResolve' + 'plus');",
+        "require.resolve('requireResolve' + x + 'plus');"
+    ],
+
+    requireEnsure: [
+        "require.ensure(['./requireEnsure'], () => {});"
+    ],
+
+    requireContext: [
+        "require.context('./requireContext');"
+    ],
+
+    tsImportRequire: [
+        "import foo = require('tsImportRequire')"
+    ]
+};
+
+export default lines;
---
+++ packages/walker/test/integrationTests.ts
@@ -0,0 +1,245 @@
+import * as babelParser from '@babel/parser';
+import walkFile from 'walker/walkFile';
+import visitors from 'visitors/visitors';
+
+const babelParserOptions: babelParser.ParserOptions = {
+    sourceType: 'module',
+    plugins: [
+        'typescript',
+        'dynamicImport',
+    ]
+};
+
+describe("import", () => {
+    it("import './import';", () => {
+        const importLine = "import './import';";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import foo1 from './importDefault';", () => {
+        const importLine = "import foo1 from './importDefault';";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import { default as foo2 } from './importDefaultRename';", () => {
+        const importLine = "import { default as foo2 } from './importDefaultRename';";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import { foo, bar } from './importSome';", () => {
+        const importLine = "import { foo, bar } from './importSome';";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import { default as foo3, bar as bar1 } from './importRenames';", () => {
+        const importLine = "import { default as foo3, bar as bar1 } from './importRenames';";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import * as foobar from './importAll';", () => {
+        const importLine = "import * as foobar from './importAll';";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("dynamicImport", () => {
+    it("import('./dynamicImport');", () => {
+        const importLine = "import('./dynamicImport');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import('./dynamicImport' + 'plus')", () => {
+        const importLine = "import('./dynamicImport' + 'plus')";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import('./dynamicImport' + x + 'wildcard')", () => {
+        const importLine = "import('./dynamicImport' + x + 'wildcard')";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("awaitDynamicImport", () => {
+    it("const async1 = async () => await import('./dynamicImportAwait');", () => {
+        const importLine = "const async1 = async () => await import('./dynamicImportAwait');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("const async2 = async () => await import('./dynamicImportAwait' + 'plus');", () => {
+        const importLine = "const async2 = async () => await import('./dynamicImportAwait' + 'plus');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("const async3 = async () => await import('./dynamicImportAwait' + x + 'wildcard');", () => {
+        const importLine = "const async3 = async () => await import('./dynamicImportAwait' + x + 'wildcard');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("dynamicImportThen", () => {
+    it("import('./dynamicImportThen').then(() => {});", () => {
+        const importLine = "import('./dynamicImportThen').then(() => {});";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import('./dynamicImportThen' + 'plus').then(() => {});", () => {
+        const importLine = "import('./dynamicImportThen' + 'plus').then(() => {});";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("import('./dynamicImportThen' + x + 'wildcard').then(() => {});", () => {
+        const importLine = "import('./dynamicImportThen' + x + 'wildcard').then(() => {});";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("require", () => {
+    it("require('./require');", () => {
+        const importLine = "require('./require');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("require('./require' + 'plus')", () => {
+        const importLine = "require('./require' + 'plus')";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("require('./require' + x + 'wildcard')", () => {
+        const importLine = "require('./require' + x + 'wildcard')";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("requireResolve", () => {
+    it("require.resolve('requireResolve');", () => {
+        const importLine = "require.resolve('requireResolve');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("require.resolve('requireResolve' + 'plus');", () => {
+        const importLine = "require.resolve('requireResolve' + 'plus');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+
+    it("require.resolve('requireResolve' + x + 'plus');", () => {
+        const importLine = "require.resolve('requireResolve' + x + 'plus');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("requireEnsure", () => {
+    it("require.ensure(['./requireEnsure'], () => {});", () => {
+        const importLine = "require.ensure(['./requireEnsure'], () => {});";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("requireContext", () => {
+    it("require.context('./requireContext');", () => {
+        const importLine = "require.context('./requireContext');";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
+
+describe("tsImportRequire", () => {
+    it("import foo = require('tsImportRequire')", () => {
+        const importLine = "import foo = require('tsImportRequire')";
+        const file = babelParser.parse(
+            importLine, babelParserOptions);
+        const result = walkFile(file, visitors);
+
+        expect(result.dependencies).toHaveLength(1);
+    })
+})
