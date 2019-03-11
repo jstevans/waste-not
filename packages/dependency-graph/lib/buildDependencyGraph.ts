7c9370bfe2dabf08186ccae811f08a1cfa7541a4
John Stevans <jstevans@users.noreply.github.com>
2019-03-11 14:13:42 -0700

    Combine configure() with main function in buildDependencyGraph

--- packages/dependency-graph/lib/buildDependencyGraph.ts
+++ packages/dependency-graph/lib/buildDependencyGraph.ts
@@ -1,35 +1,34 @@
 import { DependencyGetter, Dependencies } from "../../dependencies/lib/types";
 
-export type FileInfo<T> = {[filePath: string]: T }
+export type FileInfo<T> = { [filePath: string]: T }
+
 export type Graph = {
-    forward: FileInfo<Dependencies>,
+    forward: FileInfo<Dependencies & { dependencies: Dependencies['fileDependencies'] }>,
     reverseFiles: FileInfo<string[]>,
     reverseWildcards: FileInfo<string>
 }
 
-export default function configure(getDependencies: DependencyGetter) {
-    return function buildDependencyGraph(filePaths: string[]): Graph {
-        let filePathSet = new Set(filePaths);
+export default function buildDependencyGraph(filePaths: string[], getDependencies: DependencyGetter): Graph {
+    let filePathSet = new Set(filePaths);
 
-        return Array.from(filePathSet.values()).reduce((graph, fp) => {
-            let deps = getDependencies(fp);
+    return Array.from(filePathSet.values()).reduce((graph, fp) => {
+        let deps = getDependencies(fp);
 
-            graph.forward[fp] = {
-                ...deps,
-                dependencies: deps.fileDependencies
-            };
+        graph.forward[fp] = {
+            ...deps,
+            dependencies: deps.fileDependencies
+        };
 
-            deps.fileDependencies.forEach(fd => 
-                graph.reverseFiles[fd] = safeConcat(graph.reverseFiles[fd], fp));
+        deps.fileDependencies.forEach(fd =>
+            graph.reverseFiles[fd] = safeConcat(graph.reverseFiles[fd], fp));
 
-            deps.wildcardDependencies.forEach(wds => {
-                [...wds.aliases, ...wds.original].forEach(wd => 
-                    graph.reverseWildcards[wd] = safeConcat(graph.reverseWildcards[wd], fp))
-            });
+        deps.wildcardDependencies.forEach(wds => {
+            [...wds.aliases, ...wds.original].forEach(wd =>
+                graph.reverseWildcards[wd] = safeConcat(graph.reverseWildcards[wd], fp))
+        });
 
-            return graph;
-        }, { forward: {}, reverseFiles: {}, reverseWildcards: {} })
-    }
+        return graph;
+    }, { forward: {}, reverseFiles: {}, reverseWildcards: {} })
 }
 
 export function safeConcat<T>(list: Array<T>, element: T) {
