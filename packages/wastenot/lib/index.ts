90ba2fc131a26a70acbc3d9febb3833d7fb4f5eb
John Stevans <jstevans@users.noreply.github.com>
2019-03-27 11:33:50 -0700

    Create wastenot API

---
+++ packages/wastenot/lib/index.ts
@@ -0,0 +1,70 @@
+import * as globby from 'globby';
+import initializeCache from '../../cache/lib/index';
+import processFiles from '../../graph-hashes/lib/processFiles';
+import {
+    CacheOptions,
+    FileInfo,
+    Metadata,
+    PropertyGetter
+    } from '../../cache/lib/types';
+import { TsConfig } from '../../dependencies/lib/types';
+
+export interface WasteNotConfig {
+    baseDirectory: string,
+    cacheDir?: string,
+    files?: string[],
+    hashAlgorithm?: string,
+    mode?: {
+        read?: boolean,
+        write?: boolean,
+        overwrite?: boolean
+    },
+    tsConfig?: string | TsConfig,
+    requireJsConfig?: string,
+    webpackConfig?: string
+
+}
+export default async function wastenot(config: WasteNotConfig) {
+    let {
+        baseDirectory = process.cwd(),
+        cacheDir = '.wastenot',
+        files = ['**/*', '*'],
+        hashAlgorithm = 'sha512',
+    } = config;
+
+    let cacheMode = { read: true, write: true, overwrite: true, ...config.mode };
+
+    function isPropertyDirty(cacheProperty: any, metadata: Metadata, fileInfo: FileInfo, propertyOptions?: any) {
+        let hashKey = propertyOptions.transitive ? 'transitiveHash' : 'hash';
+        return metadata.get()[hashKey] === cacheProperty[hashKey];
+    }
+
+    function setPropertyClean(cacheProperty: any, metadata: Metadata, fileInfo: FileInfo, propertyOptions?: any) {
+        let hashKey = propertyOptions.transitive ? 'transitiveHash' : 'hash';
+        cacheProperty[hashKey] = metadata.get()[hashKey];
+    }
+
+    let cacheOptions: CacheOptions = {
+        baseDirectory: baseDirectory,
+        cacheDirPath: cacheDir,
+        mode: cacheMode
+    };
+    let _loadCache = initializeCache(cacheOptions, isPropertyDirty, setPropertyClean);
+
+    function loadCache(fileRelativePath: string): PropertyGetter {
+        const { getProperty } = _loadCache(fileRelativePath);
+        return getProperty;
+    }
+
+    let allFiles = globby.sync(files, { unique: true, onlyFiles: true });
+    let { graph, components } = await processFiles(allFiles, hashAlgorithm, _loadCache, cacheOptions);
+
+    // we want the returned value to be a function,
+    // but for those interested (e.g. dependency analyzers), 
+    // we also want to yield our graph/components
+    loadCache['graph'] = JSON.parse(JSON.stringify(graph));
+    loadCache['components'] = JSON.parse(JSON.stringify(components));
+
+
+    return loadCache;
+}
